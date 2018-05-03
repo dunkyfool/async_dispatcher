d674d7fe6eb2b0233ca97158dbcf4e9f4e7d6bb3
Jackie <jackie@ghtinc.com>
2018-05-04 01:08:11 +0800

    update status

--- docker/api/app.py
+++ docker/api/app.py
@@ -13,23 +13,18 @@ def action_parser():
              'type': str(request.args.get('type')),
              'project': str(request.args.get('project')),
              'created_date': str(datetime.datetime.now()),
+             'info': str(request.args.get('info'))
             }
     pprint(_dict)
     r.lpush('action', str(_dict))
+    r.expire('action', 200)
     return 'OK'
 
-@app.route('/info', methods=['GET'])
-def info_parser():
-    _dict = {\
-             'class': 'info',
-             'ip': str(request.args.get('ip')),
-             'type': str(request.args.get('type')),
-             'project': str(request.args.get('project')),
-             'created_date': str(datetime.datetime.now()),
-            }
-    pprint(_dict)
-    r.lpush('info', str(_dict))
-    return 'OK'
+
+@app.route('/status', methods=['GET'])
+def status():
+    return str(r.hgetall('today'))
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
--- docker/worker/worker.py
+++ docker/worker/worker.py
@@ -1,9 +1,10 @@
-import redis, time, os
+import redis, time, os, datetime
 from ast import literal_eval
 from pprint import pprint
 
 
 def listen():
+    # Listen queue to triage the task
     r = redis.StrictRedis()
     while True:
         try:
@@ -12,10 +13,17 @@ def listen():
             if queue_len > 0:
                 action = literal_eval(r.rpop('action'))
                 pprint(action)
+
                 if action['type'] == 'trigger':
                     trigger(action)
-                elif action['type'] == 'check_status':
-                    check_status(action)
+                elif action['type'] == 'initialize':
+                    initialize(action, r)
+                elif action['type'] == 'todayTaskUpdate':
+                    todayTaskUpdate(action, r)
+                elif action['type'] == 'updateInfo':
+                    updateInfo(action, r)
+                elif action['type'] == 'shutdownCheck':
+                    shutdownCheck(action, r)
             else:
                 print 'Sleep 5 seconds'
                 time.sleep(5)
@@ -27,11 +35,61 @@ def trigger(action):
     cmd = 'ssh miuser@' + \
             action['ip']+' docker run --rm --name ' + \
             action['project']+' --network host -d worker:alpha'
+    print cmd
     os.system(cmd)
+    time.sleep(1)
+    print 'trigger complete'
 
 
-def check_status(action):
-    pass
+def initialize(action, r):
+    # Archive yesterday task record
+    # Create empty today task
+    result = r.hgetall('today')
+    if result:
+        yesterday_date = (datetime.datetime.today() - datetime.timedelta(1)).strftime("%Y-%m-%d")
+        r.hmset(yesterday_date, result)
+        r.delete('today')
+        r.hmset('today',{'created_date': datetime.datetime.now()})
+    else:
+        r.delete('today')
+        r.hmset('today',{'created_date': datetime.datetime.now()})
+    print 'Initialize complete'
+
+
+def todayTaskUpdate(action, r):
+    # Update today project
+    result = r.hgetall('today')
+    result[action['project']] = {\
+                                 'ip': action['ip'],
+                                 'boot': 'Pending',
+                                 'scan': 'Pending',
+                                 'shutdown': 'Pending',
+                                }
+    r.hmset('today', result)
+    print 'todayTaskUpdate complete'
+
+
+def updateInfo(action, r):
+    # Update Success after action like Boot, Scan and shutdown
+    result = r.hgetall('today')
+    project_result = literal_eval(result[action['project']])
+    project_result[action['info']] = 'Success'
+    result[action['project']] = project_result
+    r.hmset('today',result)
+    print action['project'], action['info'], 'Success'
+
+
+def shutdownCheck(action, r):
+    # Check whether the vm is released resource
+    result = r.hgetall('today')
+    project_result = literal_eval(result[action['project']])
+    if project_result['shutdown'] == 'Pending':
+        # Force docker to stop
+        cmd = 'ssh miuser@' + \
+            action['ip']+' docker kill ' + action['project']
+        os.system(cmd)
+        print 'Force to stop', action['project']
+    print 'shutdownCheck complete'
 
 
 if __name__=='__main__':
