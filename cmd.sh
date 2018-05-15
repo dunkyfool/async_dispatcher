4233981401297f38aa640e9984b01c4c0ac65e20
Jackie <jackie@ghtinc.com>
2018-05-15 08:37:19 +0800

    cmd.sh add test command

--- cmd.sh
+++ cmd.sh
@@ -17,6 +17,40 @@ elif [ "${1}" = "build" ]; then
   cd ../worker
   sh build.sh
   cd ../../
+elif [ "${1}" = "push" ]; then
+  read -p 'Enter git comment: ' x
+  git add .
+  git commit -m "${x}"
+  git push
+
+# test command  
+elif [ "${1}" = "ini" ]; then
+  redis-cli --scan --pattern '*'
+  curl 'localhost:5000/action?ip=localhost&type=initialize'
+  sleep 5
+  redis-cli --scan --pattern '*'
+elif [ "${1}" = "update" ]; then
+  redis-cli hgetall today
+  curl 'localhost:5000/action?ip=localhost&type=todayTaskUpdate&project=ets_1.0'
+  curl 'localhost:5000/action?ip=localhost&type=todayTaskUpdate&project=ets_2.0'
+  curl 'localhost:5000/action?ip=localhost&type=todayTaskUpdate&project=ets_3.0'
+  sleep 5
+  redis-cli hgetall today
+elif [ "${1}" = "trigger" ]; then
+  curl 'localhost:5000/action?ip=localhost&type=trigger&project=ets_1.0'
+  curl 'localhost:5000/action?ip=localhost&type=trigger&project=ets_2.0'
+  curl 'localhost:5000/action?ip=localhost&type=trigger&project=ets_3.0'
+  sleep 5
+  docker ps 
+elif [ "${1}" = "info" ]; then
+  curl "localhost:5000/action?ip=localhost&type=updateInfo&project=ets_1.0&info=${2}"
+  sleep 5 
+  redis-cli hgetall today
+elif [ "${1}" = "check" ]; then
+  curl "localhost:5000/action?ip=localhost&type=shutdownCheck&project=${2}"
+  sleep 5
+  docker ps
+
 else
   docker-compose ${1}
 fi
